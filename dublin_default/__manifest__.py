17595ccc570d30184e6c3489f607f354238f2305
sistemassdeh <63413763+sistemassdeh@users.noreply.github.com>
2024-03-11 16:09:14 -0400

    Update __manifest__.py

--- dublin_default/__manifest__.py
+++ dublin_default/__manifest__.py
@@ -66,7 +66,7 @@
         'https://github.com/OCA/pos.git',
         ##'https://github.com/oca/report-print-send/tree/13.0',
         'https://github.com/OCA/report-print-send.git',
-        'https://github.com/pronexo-argentina/pos_proxy_services.git -b 13.0',
+        #'https://github.com/pronexo-argentina/pos_proxy_services.git -b 13.0',
         'https://github.com/OCA/contract.git -b 13.0',
         'https://github.com/odoomates/odooapps.git -b 13.0',
         'https://github.com/OCA/project -b 13.0',
